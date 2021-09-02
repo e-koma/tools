82e4f04ed28394e6c0721b9ecf7159c233dd62a5
yuto.komai <yuto.komai@aktsk.jp>
2021-09-02 18:54:10 +0900

    sesseion manager install追加

--- setup/mac/install_developments.sh
+++ setup/mac/install_developments.sh
@@ -56,8 +56,14 @@ tfenv install "$TF1_VERSION"
 tfenv use "$TF0_VERSION"
 tfenv list
 
-# パスワードが求められる
+# ここから先、パスワードが求められる
 # aws-cli
 curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg"
 sudo installer -pkg AWSCLIV2.pkg -target /
 rm "AWSCLIV2.pkg"
+
+# session manager plugin
+curl "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/mac/sessionmanager-bundle.zip" -o "sessionmanager-bundle.zip"
+unzip sessionmanager-bundle.zip
+sudo ./sessionmanager-bundle/install -i /usr/local/sessionmanagerplugin -b /usr/local/bin/session-manager-plugin
+rm "sessionmanager-bundle.zip"
