1f3a7f50cb34022ec972af8338e643b05c294e7e
yuto.komai <yuto.komai@aktsk.jp>
2021-09-02 16:43:19 +0900

    mac settings追加

--- setup/mac/install_mac_settings.sh
+++ setup/mac/install_mac_settings.sh
@@ -5,3 +5,8 @@ defaults write -g InitialKeyRepeat -int 10 # normal minimum is 15 (225 ms)
 defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
 defaults write -g com.apple.trackpad.scaling 3
 defaults write -g com.apple.mouse.scaling 1.5
+
+# 隠しファイル表示
+defaults write com.apple.finder AppleShowAllFiles YES
+
+sudo xcode-select --install
