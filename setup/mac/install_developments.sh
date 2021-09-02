83095311086360c6a816e628465f12683a5b4665
yuto.komai <yuto.komai@aktsk.jp>
2021-09-02 18:42:12 +0900

    language install設定追加

---
+++ setup/mac/install_developments.sh
@@ -0,0 +1,57 @@
+#!/bin/bash -e
+
+RUBY_VERSION=2.7.4
+PYTHON39_VERSION=3.9.7
+PYTHON38_VERSION=3.8.9
+PYTHON37_VERSION=3.7.11
+GO_VERSION=1.16.7
+TF1_VERSION=1.0.5
+TF0_VERSION=0.14.5
+
+function install_lang () {
+  local env_command="$1"
+  local version="$2"
+  "$env_command" install "$version"
+  "$env_command" global "$version"
+  "$env_command" rehash
+
+  echo "****************"
+  echo " $env_command versions "
+  echo "****************"
+  "$env_command" versions
+}
+
+function check_version () {
+  local language="$1"
+  echo "****************"
+  echo " $language version "
+  echo "****************"
+
+  case "$language" in
+    go ) go version ;;
+    * ) "$language" --version ;;
+  esac
+}
+
+# Ruby
+install_lang rbenv "$RUBY_VERSION"
+check_version ruby
+gem install bundler
+
+# Python
+install_lang pyenv "$PYTHON39_VERSION"
+install_lang pyenv "$PYTHON38_VERSION"
+install_lang pyenv "$PYTHON37_VERSION"
+check_version python
+
+# Go
+install_lang goenv "$GO_VERSION"
+check_version go
+export GOENV_DISABLE_GOPATH=1
+go env GOPATH
+
+# Terraform
+tfenv install "$TF0_VERSION"
+tfenv install "$TF1_VERSION"
+tfenv use "$TF0_VERSION"
+tfenv list
