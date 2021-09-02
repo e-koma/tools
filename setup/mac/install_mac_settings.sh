b34b0767559afc603fdc9d73a9f3a76b0dca026f
yuto.komai <yuto.komai@aktsk.jp>
2021-09-02 14:04:33 +0900

    mac設定追加

---
+++ setup/mac/install_mac_settings.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -e
+
+# mac performance
+defaults write -g InitialKeyRepeat -int 10 # normal minimum is 15 (225 ms)
+defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
+defaults write -g com.apple.trackpad.scaling 3
+defaults write -g com.apple.mouse.scaling 1.5
