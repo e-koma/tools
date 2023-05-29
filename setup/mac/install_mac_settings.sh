3864d6af915047cb826dc2db492567852424a4e7
yuto.komai <yuto.komai@aktsk.jp>
2023-05-29 13:44:06 +0900

    mac設定追加

--- setup/mac/install_mac_settings.sh
+++ setup/mac/install_mac_settings.sh
@@ -1,12 +1,15 @@
 #!/bin/bash -e
 
-# NOTE: AppStoreからXCode install済みであること
+- Macのパスワード変更
+- AppStoreからXCode install
 
 # mac performance
 defaults write -g InitialKeyRepeat -int 10 # normal minimum is 15 (225 ms)
 defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
-defaults write -g com.apple.trackpad.scaling 3
+defaults write -g com.apple.trackpad.scaling 7.0
 defaults write -g com.apple.mouse.scaling 1.5
+defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
+defaults write com.apple.dock orientation -string 'right'
 
 # 隠しファイル表示
 defaults write com.apple.finder AppleShowAllFiles YES
